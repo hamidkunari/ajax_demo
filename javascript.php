5d26bea83cc2021562bce92ea68b4d10918630f2
hamid <hamidkunari94@gmail.com>
2020-07-04 09:22:16 -0700

    master

---
+++ javascript.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+    if(isset($_POST)) {
+    
+    echo $_POST['username'];
+        
+        echo "<br>";
+        
+        
+    echo $_POST['password'];
+    
+    
+    }
+
+
+
+
+
+?>
